41a01f0f3a0af2a226b3b1a17b18fe730fd12b61
Nurul Asyifa <nurulasyifaa4@gmail.com>
2026-04-30 01:24:02 +0700

    refactor: implement blade layouts and partials for views

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">🎬 Movie App</a>
+        <div class="navbar-nav ms-auto">
+            <a class="nav-link" href="/">Home</a>
+            <a class="nav-link" href="/movies/data">Data Movie</a>
+            <a class="nav-link" href="/movies/create">Tambah Movie</a>
+        </div>
+    </div>
+</nav>
